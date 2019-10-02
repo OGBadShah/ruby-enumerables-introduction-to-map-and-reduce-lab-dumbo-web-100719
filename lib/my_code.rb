4c2d933ab6b3120ad6413863fa123ef420b660ae
iamnabeel2 <iamnabeel2@gmail.com>
2019-10-02 04:11:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,31 @@
 # My Code here....
+map_to_negativize(source_array)
+new_array = []
+i = 0
+while i < source_array.length do
+  new_array.push(source_array[i] * -1)
+  i +=1
+end
+return new_array
+end
+
+map_to_no_change(source_array)
+new_array = []
+i = 0
+while i < source_array.length do
+  new_array.push(source_array[i])
+  i +=1
+
+end
+  return new_array
+end
+
+map_to_double(source_array)
+new_array = []
+i = 0
+while i < source_array.length do
+  new_array.push(source_array[i] *2)
+  i +=1
+end
+return new_array
+end
